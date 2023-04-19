5683dd75d0739207118ca58ebbf2f95d78d550d8
Yujin <jyj15938@gmail.com>
2023-04-19 23:16:10 +0900

    [setting] db.json 모델링

---
+++ toyproject/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import IntroduceCard from './components/IntroduceCard';
+
+function App() {
+  return (
+    <div className="App">
+      <IntroduceCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ toyproject/src/components/IntroduceCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function IntroduceCard() {
+  return (
+    <section>
+      <h1>Introduce Card</h1>
+      <div></div>
+
+      <div>이곳은 리뷰칸</div>
+      <button>next</button>
+      <button>next</button>
+    </section>
+  );
+}
